c5c6b5d0216780fdd8e3dfa94a7b205545756e44
Jason Merrill <jason@desmos.com>
2013-11-16 12:28:31 -0800

    Change the name of PowerSeriesType to Series.

--- src/PowerSeries.jl
+++ src/PowerSeries.jl
@@ -2,50 +2,50 @@ module PowerSeries
 
 import Base: diff, +, -, *, /, sin, cos, exp
 
-immutable PowerSeriesType{T<:Real} <: Number
+immutable Series{T<:Real} <: Number
   re::T
-  ep::Union(T, PowerSeriesType{T})
+  ep::Union(T, Series{T})
 end
 
-+(a::PowerSeriesType, b::PowerSeriesType) = PowerSeriesType(a.re + b.re, a.ep + b.ep)
-+(a::Real, b::PowerSeriesType) = PowerSeriesType(a + b.re, b.ep)
-+(a::PowerSeriesType, b::Real) = PowerSeriesType(a.re + b, a.ep)
++(a::Series, b::Series) = Series(a.re + b.re, a.ep + b.ep)
++(a::Real, b::Series) = Series(a + b.re, b.ep)
++(a::Series, b::Real) = Series(a.re + b, a.ep)
 
--(a::PowerSeriesType) = PowerSeriesType(-a.re, -a.ep)
--(a::PowerSeriesType, b::PowerSeriesType) = PowerSeriesType(a.re - b.re, a.ep - b.ep)
--(a::Real, b::PowerSeriesType) = PowerSeriesType(a - b.re, -b.ep)
--(a::PowerSeriesType, b::Real) = PowerSeriesType(a.re - b, -a.ep)
+-(a::Series) = Series(-a.re, -a.ep)
+-(a::Series, b::Series) = Series(a.re - b.re, a.ep - b.ep)
+-(a::Real, b::Series) = Series(a - b.re, -b.ep)
+-(a::Series, b::Real) = Series(a.re - b, -a.ep)
 
-*{T<:Real}(a::PowerSeriesType{T}, b::PowerSeriesType{T}) = PowerSeriesType(
+*{T<:Real}(a::Series{T}, b::Series{T}) = Series(
   a.re*b.re,
   a.re*b.ep + b.re*a.ep +
-  restrict(PowerSeriesType(zero(T), a.ep*b.ep))
+  restrict(Series(zero(T), a.ep*b.ep))
 )
-*(a::Real, b::PowerSeriesType) = PowerSeriesType(a*b.re, a*b.ep)
-*(a::PowerSeriesType, b::Real) = PowerSeriesType(b*a.re, b*a.ep)
+*(a::Real, b::Series) = Series(a*b.re, a*b.ep)
+*(a::Series, b::Real) = Series(b*a.re, b*a.ep)
 
-restrict(p::PowerSeriesType) = _restrict(p.re, p.ep)
+restrict(p::Series) = _restrict(p.re, p.ep)
 
 _restrict{T<:Real}(r::T, e::T) = r
-_restrict{T<:Real}(r::T, e::PowerSeriesType{T}) = PowerSeriesType(r, _restrict(e.re, e.ep))
+_restrict{T<:Real}(r::T, e::Series{T}) = Series(r, _restrict(e.re, e.ep))
 
 diff{T<:Real}(p::T) = zero(T)
-diff{T<:Real}(p::PowerSeriesType{T}) = _diff(p.ep, one(T))
+diff{T<:Real}(p::Series{T}) = _diff(p.ep, one(T))
 
 _diff{T<:Real}(p::T, n::T) = n*p
-_diff{T<:Real}(p::PowerSeriesType{T}, n::T) = PowerSeriesType(n*p.re, _diff(p.ep, n + one(T)))
+_diff{T<:Real}(p::Series{T}, n::T) = Series(n*p.re, _diff(p.ep, n + one(T)))
 
-pint{T<:Real}(p::T) = PowerSeriesType(zero(T), p)
-pint{T<:Real}(p::PowerSeriesType{T}) = PowerSeriesType(zero(T), _pint(p.re, p.ep, one(T)))
+pint{T<:Real}(p::T) = Series(zero(T), p)
+pint{T<:Real}(p::Series{T}) = Series(zero(T), _pint(p.re, p.ep, one(T)))
 
-_pint{T<:Real}(r::T, e::T, n::T) = PowerSeriesType(r/n, e/(n + one(T)))
-_pint{T<:Real}(r::T, e::PowerSeriesType{T}, n::T) =
-  PowerSeriesType(r/n, _pint(e.re, e.ep, n + one(T)))
+_pint{T<:Real}(r::T, e::T, n::T) = Series(r/n, e/(n + one(T)))
+_pint{T<:Real}(r::T, e::Series{T}, n::T) =
+  Series(r/n, _pint(e.re, e.ep, n + one(T)))
 
-exp(p::PowerSeriesType) = exp(p.re) + pint(diff(p)*exp(restrict(p)))
-sin(p::PowerSeriesType) = sin(p.re) + pint(diff(p)*cos(restrict(p)))
-cos(p::PowerSeriesType) = cos(p.re) - pint(diff(p)*sin(restrict(p)))
+exp(p::Series) = exp(p.re) + pint(diff(p)*exp(restrict(p)))
+sin(p::Series) = sin(p.re) + pint(diff(p)*cos(restrict(p)))
+cos(p::Series) = cos(p.re) - pint(diff(p)*sin(restrict(p)))
 
-export PowerSeriesType, pint, restrict
+export Series, pint, restrict
 
 end
