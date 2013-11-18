bc6e0fed38af3c93bee6c40e9546f770ab31b37a
Jason Merrill <jason@desmos.com>
2013-11-17 17:27:59 -0800

    Generalize Series from reals to general number field.
    This causes a couple of conflicts with Complex. Will have to figure out how to resolve those.

--- src/PowerSeries.jl
+++ src/PowerSeries.jl
@@ -2,64 +2,63 @@ module PowerSeries
 
 import Base: diff, +, -, *, /, ^, sin, cos, exp, log, show, showcompact
 
-immutable Series{T<:Real} <: Number
+immutable Series{T<:Number} <: Number
   re::T
   ep::Union(T, Series{T})
 end
 
-Series(a, b...) = Series(a, Series(b...))
+Series(a, b, c...) = Series(a, Series(b, c...))
 
 +(a::Series, b::Series) = Series(a.re + b.re, a.ep + b.ep)
-+(a::Real, b::Series) = Series(a + b.re, b.ep)
-+(a::Series, b::Real) = Series(a.re + b, a.ep)
++(a::Number, b::Series) = Series(a + b.re, b.ep)
++(a::Series, b::Number) = Series(a.re + b, a.ep)
 
 -(a::Series) = Series(-a.re, -a.ep)
 -(a::Series, b::Series) = Series(a.re - b.re, a.ep - b.ep)
--(a::Real, b::Series) = Series(a - b.re, -b.ep)
--(a::Series, b::Real) = Series(a.re - b, -a.ep)
+-(a::Number, b::Series) = Series(a - b.re, -b.ep)
+-(a::Series, b::Number) = Series(a.re - b, -a.ep)
 
-*{T<:Real}(a::Series{T}, b::Series{T}) = Series(
+*{T<:Number}(a::Series{T}, b::Series{T}) = Series(
   a.re*b.re,
   a.re*b.ep + b.re*a.ep +
   restrict(Series(zero(T), a.ep*b.ep))
 )
-*(a::Real, b::Series) = Series(a*b.re, a*b.ep)
-*(a::Series, b::Real) = Series(b*a.re, b*a.ep)
+*(a::Number, b::Series) = Series(a*b.re, a*b.ep)
+*(a::Series, b::Number) = Series(b*a.re, b*a.ep)
 
 promote_depth{T}(a::Series{T}, b::Series{T}) = Series(b.re, promote_depth(a.ep, b.ep))
 promote_depth{T}(a::Series{T}, b::T) = Series(b, promote_depth(a.ep, zero(T)))
 promote_depth{T}(a::T, b::Series{T}) = b
 promote_depth{T}(a::T, b::T) = b
 
-function /{T<:Real}(a::Series{T}, b::Series{T})
+function /{T<:Number}(a::Series{T}, b::Series{T})
   rb = promote_depth(a.ep, b) # same as restrict(promote_depth(a, b))
   a/b.re - a*pint(diff(b)/(rb*rb))
 end
-/(a::Series, b::Real) = Series(a.re/b, a.ep/b)
-function /(a::Real, b::Series)
+/{T<:Number}(a::Series{T}, b::T) = Series(a.re/b, a.ep/b)
+function /{T<:Number}(a::T, b::Series{T})
   rb = restrict(b)
   a/b.re - a*pint(diff(b)/(rb*rb))
 end
 
-# TODO, causes warnings
-# ^{T<:Real}(a::Series{T}, b::Real) = Series(a.re^b, b*a.ep^(b - one(T)))
+^{T<:Number}(a::Series{T}, b::Number) = Series(a.re^b, b*a.ep^(b - one(T)))
 
 restrict(p::Series) = _restrict(p.re, p.ep)
 
-_restrict{T<:Real}(r::T, e::T) = r
-_restrict{T<:Real}(r::T, e::Series{T}) = Series(r, _restrict(e.re, e.ep))
+_restrict{T<:Number}(r::T, e::T) = r
+_restrict{T<:Number}(r::T, e::Series{T}) = Series(r, _restrict(e.re, e.ep))
 
-diff{T<:Real}(p::T) = zero(T)
-diff{T<:Real}(p::Series{T}) = _diff(p.ep, one(T))
+diff{T<:Number}(p::T) = zero(T)
+diff{T<:Number}(p::Series{T}) = _diff(p.ep, one(T))
 
-_diff{T<:Real}(p::T, n::T) = n*p
-_diff{T<:Real}(p::Series{T}, n::T) = Series(n*p.re, _diff(p.ep, n + one(T)))
+_diff{T<:Number}(p::T, n::T) = n*p
+_diff{T<:Number}(p::Series{T}, n::T) = Series(n*p.re, _diff(p.ep, n + one(T)))
 
-pint{T<:Real}(p::T) = Series(zero(T), p)
-pint{T<:Real}(p::Series{T}) = Series(zero(T), _pint(p.re, p.ep, one(T)))
+pint{T<:Number}(p::T) = Series(zero(T), p)
+pint{T<:Number}(p::Series{T}) = Series(zero(T), _pint(p.re, p.ep, one(T)))
 
-_pint{T<:Real}(r::T, e::T, n::T) = Series(r/n, e/(n + one(T)))
-_pint{T<:Real}(r::T, e::Series{T}, n::T) =
+_pint{T<:Number}(r::T, e::T, n::T) = Series(r/n, e/(n + one(T)))
+_pint{T<:Number}(r::T, e::Series{T}, n::T) =
   Series(r/n, _pint(e.re, e.ep, n + one(T)))
 
 exp(p::Series) = exp(p.re) + pint(diff(p)*exp(restrict(p)))
@@ -67,13 +66,13 @@ sin(p::Series) = sin(p.re) + pint(diff(p)*cos(restrict(p)))
 cos(p::Series) = cos(p.re) - pint(diff(p)*sin(restrict(p)))
 log(p::Series) = log(p.re) + pint(diff(p)/restrict(p))
 
-function _show_rest{T<:Real}(io::IO, method, r::T, e::Series{T})
+function _show_rest{T<:Number}(io::IO, method, r::T, e::Series{T})
   method(r)
   print(io, ",")
   _show_rest(io, method, e.re, e.ep)
 end
 
-function _show_rest{T<:Real}(io::IO, method, r::T, e::T)
+function _show_rest{T<:Number}(io::IO, method, r::T, e::T)
   method(r)
   print(io, ",")
   method(e)
